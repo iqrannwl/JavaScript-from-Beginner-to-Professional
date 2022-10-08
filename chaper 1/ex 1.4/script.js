298f953580ae04209478e979729eafaba2533873
iqrannwl <iqrannwl@gmail.com>
2022-10-09 01:20:07 +0500

    JavaScript-from-Beginner-to-Professional

---
+++ chaper 1/ex 1.4/script.js
@@ -0,0 +1,8 @@
+alert("Please see the console for orignal output")
+
+// This is a synetex creating a variable in javascript
+let a = 10
+
+// This is  function  available in javascript use to show whatever you pass
+console.log(a)
+
